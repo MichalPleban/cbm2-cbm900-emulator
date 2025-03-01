99a96816f96cba3d0697f4944bb145fb5d87043d
Michal Pleban <michalpleban@gmail.com>
2025-03-01 13:46:00 +0100

    Emulation load at boot

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -34,9 +34,13 @@
 .endmacro
 
 boot_emulation:
-        ; Init the SD card and look for the emulation.file
+        sei
+        
+        ; Init the SD card and look for the emulation file
         jsr fat32_init
-        bcs @error
+        bcc @initialized
+        jmp @error
+@initialized:
         lda #<emul_filename
         ldy #>emul_filename
         jsr fat32_find_file
@@ -46,38 +50,90 @@ boot_emulation:
         jsr fat32_scan_file
         bcs @error        
         
-        ; Load the file into memory
+        ; Load file into memory
         lda #0
         sta fat32_pointers+FILE_SECTOR
         sta fat32_pointers+FILE_SECTOR+1
-        sta fat32_pointers+FILE_SECTOR+2
-        sta fat32_pointers+FILE_SECTOR+3
+        sta fat32_ptr_1+1
+        lda #1
+        sta fat32_ptr_1
         
 @loop:
+        ; Load one sector into buffer
         lda fat32_pointers+FILE_SECTOR
         sta sd_sector
         lda fat32_pointers+FILE_SECTOR+1
         sta sd_sector+1
-        lda fat32_pointers+FILE_SECTOR+2
+        lda #0
         sta sd_sector+2
-        lda fat32_pointers+FILE_SECTOR+3
+        lda #0
         sta sd_sector+3
         lda #<file_mapping
         ldy #>file_mapping
         jsr fat32_translate
+        bcc @continue
+        cmp #$52
+        beq @ok
+        bne @error
+@continue:
         jsr fat32_set_buffer
-        jsr fat32_load_sector
+        BANK_RESTORE
+        jsr sd_read
+        BANK_SAVE
+        bcs @error
+        
+        ; Copy buffer to the RAM
+        lda #$01
+        sta $01
+        ldy #0
+        lda fat32_ptr_1+1
+        bne @not_first
+        iny
+        iny
+@not_first:
+@loop2:
+        lda fat32_buffer,y
+        sta (fat32_ptr_1),y
+        iny
+        bne @loop2
+        inc fat32_ptr_1+1
+@loop3:
+        lda fat32_buffer+256,y
+        sta (fat32_ptr_1),y
+        iny
+        bne @loop3
+        lda #$0F
+        sta $01
+        inc fat32_ptr_1+1
+        
+        ; Increase file pointer        
+        inc fat32_pointers+FILE_SECTOR
+        bne @loop
+        inc fat32_pointers+FILE_SECTOR+1
+        bne @loop
+        lda #$52
+        
 @error:
-        sta $1FFF
+        ; Report error and return
+        cli
         rts
+        
+@ok:
+        ; Start the emulation code at $010400
+        lda #$A9
+        sta $03FC
+        lda #$01    ; LDA #$01
+        sta $03FD
+        lda #$85
+        sta $03FE
+        lda #$00    ; STA $00
+        sta $03FF
+        jmp $03FC
 
 sd_output:
-        sta CHIPSET_BASE + SD_CARD
+        sta CHIPSET_BASE + REG_SD_CARD
         nop
-        nop
-        nop
-        nop
-        lda CHIPSET_BASE + SD_CARD
+        lda CHIPSET_BASE + REG_SD_CARD
         rts
 
 sd_read_loop:
@@ -115,7 +171,7 @@ sd_write_loop:
 @loop:
         ldy sd_loop
         lda (sd_ptr),y
-        sta CHIPSET_BASE + SD_CARD
+        sta CHIPSET_BASE + REG_SD_CARD
         iny
         sty sd_loop
         bne @loop
